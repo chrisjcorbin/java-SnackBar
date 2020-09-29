69b1ef4a488a8657e08679ce6433346076c2e3be
Chris <cjcorbin@gmail.com>
2020-09-28 21:07:52 -0700

    Snack.java

--- src/snackbarApp/Snack.java
+++ src/snackbarApp/Snack.java
@@ -2,5 +2,81 @@ package snackbarApp;
 
 public class Snack
 {
+	private static int maxId = 0;
+	private int id;
+	private String name;
+	private int quantity;
+	private double cost;
+	private int vendingMachineId;
 
+	public Snack(String name, int quantity, double cost, int vendingMachineId)
+	{
+		maxId++;
+		id = maxId;
+
+		this.name = name;
+		this.quantity = quantity;
+		this.cost = cost;
+		this.vendingMachineId = vendingMachineId;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public double getCost()
+	{
+		return cost;
+	}
+
+	public void setCost(double cost)
+	{
+		this.cost = cost;
+	}
+
+	public int getVendingMachineId()
+	{
+		return vendingMachineId;
+	}
+
+	public void setVendingMachineId(int vendingMachineId)
+	{
+		this.vendingMachineId = vendingMachineId;
+	}
+
+	public int getQuantity()
+	{
+		return quantity;
+	}
+
+	public void addQuantity(int addQuantity)
+	{
+		this.quantity += addQuantity;
+	}
+
+	public void buySnack(int buySnack)
+	{
+		this.quantity -= buySnack;
+	}
+
+	public double totalCost(int num)
+	{
+		this.cost * num;
+	}
 }
