6306f5b119b8a2fc53983bb81c345044aa195d2a
Chris <cjcorbin@gmail.com>
2020-09-28 21:17:37 -0700

    VendingMachine.java

--- src/snackbarApp/VendingMachine.java
+++ src/snackbarApp/VendingMachine.java
@@ -2,5 +2,35 @@ package snackbarApp;
 
 public class VendingMachine
 {
+	private static int maxId = 0;
+	private int id;
+	private String name;
 
+	public Snack(String name)
+	{
+		maxId++;
+		id = maxId;
+
+		this.name = name;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 }
